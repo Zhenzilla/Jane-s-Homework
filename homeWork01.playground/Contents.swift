1c072f29591b551537a4d18b83e00b3202459e54
Evgeniya Bednova <e.bednova@corp.vk.com>
2024-02-11 18:25:18 +0300

    Jane-homework01
    Multi-Line String Literal Interpolation was added
    Grammar fixes

--- homeWork01.playground/Contents.swift
+++ homeWork01.playground/Contents.swift
@@ -3,6 +3,15 @@ let studentSurname = "Ivanov"
 let studentAge = 28
 let studentSex = "male"
 
-print("Stident's name is \(studentName).\nStudent's surname is \(studentSurname).\nStudent's age is \(studentAge).\nStudent's sex is \(studentSex).\n")
+print("Student's name is \(studentName).\nStudent's surname is \(studentSurname).\nStudent's age is \(studentAge).\nStudent's sex is \(studentSex).\n")
 
-print("Stident's name is \(studentName). Student's surname is \(studentSurname). Student's age is \(studentAge). Student's sex is \(studentSex).")
+print("Student's name is \(studentName). Student's surname is \(studentSurname). Student's age is \(studentAge). Student's sex is \(studentSex).\n")
+
+
+let multiLineString = """
+                      Student's name is \(studentName).
+                      Student's surname is \(studentSurname).
+                      Student's age is \(studentAge).
+                      Student's sex is \(studentSex).
+                      """
+print(multiLineString)
