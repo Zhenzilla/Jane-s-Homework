f64867fb6053492e99a77ab433b1a3742cac2218
Evgeniya Bednova <e.bednova@corp.vk.com>
2024-02-11 17:43:39 +0300

    Jane-homework01
    домашка с выводом инфы студента с помощью констант

---
+++ homeWork01.playground/Contents.swift
@@ -0,0 +1,8 @@
+let studentName = "Ivan"
+let studentSurname = "Ivanov"
+let studentAge = 28
+let studentSex = "male"
+
+print("Stident's name is \(studentName).\nStudent's surname is \(studentSurname).\nStudent's age is \(studentAge).\nStudent's sex is \(studentSex).\n")
+
+print("Stident's name is \(studentName). Student's surname is \(studentSurname). Student's age is \(studentAge). Student's sex is \(studentSex).")
